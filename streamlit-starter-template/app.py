3478cea126deeed65bd171a247c9e256a4832d02
anAly-t <jboggs24@bethelcollege.edu>
2024-09-28 06:21:17 -0500

    Update app.py

--- streamlit-starter-template/app.py
+++ streamlit-starter-template/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #sets the page to wide layout which is not automatic
 st.set_page_config(layout = 'wide')
 
-st.title("Streamlit Template")
+st.title("My Card Collector App")
 
 if st.button("Click Me"):
     st.write("👋 Hi")
